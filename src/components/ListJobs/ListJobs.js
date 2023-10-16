77efc99244df512b9ff0f70a5d4a9af7061b6e8f
MartinMGomezVega <martin_gomezvega@hotmail.com>
2023-10-16 00:45:18 -0300

    modal de la descripcion del empleo

--- src/components/ListJobs/ListJobs.js
+++ src/components/ListJobs/ListJobs.js
@@ -6,6 +6,7 @@ import moment from 'moment';
 import { getUserAPI } from '../../API/user';
 import { API_HOST } from '../../utils/constant';
 import { replaceURLWithHTMLLinks } from '../../utils/functions';
+import DescriptionModal from '../../components/Modal/JobsModal/DescriptionModal'
 
 import "./ListJobs.scss";
 
@@ -26,8 +27,10 @@ function Job(props){
     const { job } = props;
     const [userInfo, setUserInfo] = useState(null); // Guardar la informacion del usuario
     const [jobFormLoading, setJobFormLoading] = useState(false);
+    const [descriptionModal, setDescriptionModal] = useState('');
+    const [showModal, setShowModalJobs] = useState(false); // Modal para la descripción del empleo
 
-    console.log(job.datePublication)
+    console.log(props)
 
     // Obtener el avatar para mostrar en la publicacion
     useEffect(() => {
@@ -35,17 +38,21 @@ function Job(props){
           setUserInfo(response);
         });
     }, [job]);
-    
+
+    const showDescripcion = (descripcion) => {
+        setDescriptionModal(descripcion);
+        setShowModalJobs(true);
+    };
 
     return (
         <div className='job'>
             <div>
                 <div className='position'>
-                    {job?.position}  
+                    {job?.position}
                     <span>{moment(job.datePublication).calendar()}</span>
                 </div>
                 <div className='company'>
-                    {job?.company} 
+                    {job?.company}
                     <Button variant="primary" type="submit" className='job_button'>Postularme
                         {/* {!jobFormLoading ? "Postularme" : <Spinner animation="border" />} */}
                     </Button>
@@ -55,9 +62,9 @@ function Job(props){
                 </div>
                 <div className='description'>
                     {/* {userInfo?.description}  */}
-                    Descripción +
+                    <button onClick={() => showDescripcion(job?.description)}>Ver descripción</button>
+                    <DescriptionModal show={showModal} setShow={setShowModalJobs} description={job?.description} />
                 </div>
-
 
                 <div>
                     {/* Mostrar publicaciones renderizadas si hay links */}
@@ -66,5 +73,4 @@ function Job(props){
             </div>
         </div>
     );
- 
 }
---
+++ src/components/Modal/JobsModal/DescriptionModal/DescriptionModal.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { Close } from "../../../../utils/Icons";
+
+import './DescriptionModal.scss'
+
+function DescriptionModal(props) {
+    const { show, description, setShow } = props;
+    const [formData, setFormData] = useState(initialFormValue());
+
+    console.log(props);
+
+    const handleClose = () => {
+        // Restablecer los valores del formulario al cerrar el modal
+        setFormData(initialFormValue());
+        setShow(false); // Para cerrar el modal
+    }
+
+    return (
+        <Modal className='description-job' show={show} onHide={handleClose} centered size='lg'>
+            <Modal.Header>
+                <Modal.Title>
+                    <Close onClick={handleClose} />
+                </Modal.Title>
+            </Modal.Header>
+
+            <h2>Descripción del empleo</h2>
+
+            <Modal.Body>
+                {description}
+            </Modal.Body>
+            {/* <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                    Cerrar
+                </Button>
+            </Modal.Footer> */}
+        </Modal>
+    );
+}
+
+// initialFormValue: Inicializar los campos del formulario de publicación del puesto laboral
+function initialFormValue() {
+    return {
+        description:""
+    };
+}
+
+export default DescriptionModal;
